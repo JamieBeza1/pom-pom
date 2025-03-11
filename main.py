613fa17aaeff15c4911b23ff675a5a16b21c67bd
JamieBeza1 <jamie.beza@gmail.com>
2025-03-11 16:02:52 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import requests
+import os
 import json
 import xml.etree.ElementTree as ET
-
 
 class Downloader:
     def __init__(self):
         self.pom = "https://repo1.maven.org/maven2/org/apache/flink/flink-parent/1.20.1/flink-parent-1.20.1.pom"
-        self.filename = "/home/jamie/Documents/maven scripts/flink-parent-1.20.1.pom"
+        self.filename = "/home/beza/Downloads/pom-pom-main/flink-parent-1.20.1.pom"
         self.deps = []
         self.plugins = []
+        self.base = "https://repo1.maven.org/maven2/"
+        self.download_location = "/home/beza/Downloads/pom-pom-main/pkg/"
 
     def download(self):
         print(self.pom)
         try:
             response = requests.get(self.pom, allow_redirects=True)
             self.write_output(self.filename, response.content)
-            #open("pom","wb").write(response.content)
             print("success")
         except BaseException as be:
             print(f"Something went wrong while downloading: {self.pom}\nThe following exception was raised: {be}")
             exit(1)
     
-    def write_output(self,filename, output):
+    def write_output(self, filename, output):
         with open(filename, "wb") as file:
             file.write(output)
         
@@ -32,52 +33,71 @@ class Downloader:
     def extract_plugins(self, data, properties):
         root = ET.fromstring(data)
         namespace = {'': 'http://maven.apache.org/POM/4.0.0'}
-        plugins = root.findall(".//plugin",namespace)
 
-        for plugin in plugins:
-            exclusions = plugin.findall(".//exclusions/exclusion",namespace)
+        plugin_elements = root.findall(".//build/pluginManagement/plugins/plugin", namespace) + root.findall(".//build/plugins/plugin", namespace)
+
+        for plugin in plugin_elements:
+            artifact_id = plugin.find('artifactId', namespace).text if plugin.find('artifactId', namespace) is not None else "N/A"
+            version = plugin.find('version', namespace).text if plugin.find('version', namespace) is not None else "N/A"
+
+            if version == "N/A":
+                parent_version = plugin.find('parent/version', namespace)
+                if parent_version is not None:
+                    version = parent_version.text
+
+            artifact_id = self.resolve_placeholder(artifact_id, properties)
+            version = self.resolve_placeholder(version, properties)
+
+            exclusions = plugin.findall(".//exclusions/exclusion", namespace)
             if exclusions:
-                print(f"Skipping plugin '{plugin.find('artifactId', namespace).text}' due to exclusions.")
+                print(f"Skipping plugin '{artifact_id}' due to exclusions.")
                 continue
-            
-            a_id = plugin.find('artifactId', namespace).text if plugin.find('artifactId', namespace) is not None else "N/A"
-            v = plugin.find('version', namespace).text if plugin.find('version', namespace) is not None else "N/A"
-            
-            v = self.resolve_placeholder(v, properties)
-            a_id = self.resolve_placeholder(a_id, properties)
 
-            self.plugins.append({
-                'artifactId':a_id,
-                'version':v
-            })
-        
+            plugin_info = {
+                'artifactId': artifact_id,
+                'version': version
+            }
+
+            if plugin_info not in self.plugins:
+                self.plugins.append(plugin_info)
+
         for plugin in self.plugins:
             print(f"Plugin: {plugin['artifactId']}, Version: {plugin['version']}")
-        
+
+
+
     def extract_dependencies(self, data, properties):
         root = ET.fromstring(data)
         namespace = {'': 'http://maven.apache.org/POM/4.0.0'}
         dependencies = root.findall(".//dependency", namespace)
+        excluded_deps = []
 
         for dep in dependencies:
-            exclusions = dep.findall(".//exclusions/exclusion",namespace)
+            exclusions = dep.findall(".//exclusions/exclusion", namespace)
             should_skip = False
 
             if exclusions:
                 for exclusion in exclusions:
                     exclusion_group_id = exclusion.find('groupId', namespace).text if exclusion.find('groupId', namespace) is not None else "N/A"
                     exclusion_artifact_id = exclusion.find('artifactId', namespace).text if exclusion.find('artifactId', namespace) is not None else "N/A"
-
-                    if exclusion_group_id == dep.find('groupId', namespace).text and exclusion_artifact_id == dep.find('artifactId', namespace).text:
-                        print(f"Skipping dependency '{dep.find('artifactId', namespace).text}' due to exclusions.")
-                        should_skip = True
-                        break  
+                    
+                    excluded_deps.append({
+                        'groupId': exclusion_group_id,
+                        'artifactId': exclusion_artifact_id,
+                        'message': f"Excluded: {exclusion_artifact_id} from {dep.find('artifactId', namespace).text}"
+                    })
+                    
+                    should_skip = True
+                    break
 
             if not should_skip:
-                group_id = dep.find('groupId', namespace).text if dep.find('groupId', namespace) is not None else "N/A1"
-                artifact_id = dep.find('artifactId', namespace).text if dep.find('artifactId', namespace) is not None else "N/A2"
-                version = dep.find('version', namespace).text if dep.find('version', namespace) is not None else "N/A3"
-                #scope = dep.find('scope', namespace).text if dep.find('scope', namespace) is not None else "N/A"
+                group_id = dep.find('groupId', namespace).text if dep.find('groupId', namespace) is not None else None
+                artifact_id = dep.find('artifactId', namespace).text if dep.find('artifactId', namespace) is not None else None
+                version = dep.find('version', namespace).text if dep.find('version', namespace) is not None else None
+
+                if not group_id or not artifact_id or not version:
+                    print(f"Missing information for dependency: groupId={group_id}, artifactId={artifact_id}, version={version}")
+                    continue
 
                 artifact_id = self.resolve_placeholder(artifact_id, properties)
                 version = self.resolve_placeholder(version, properties)
@@ -88,12 +108,13 @@ class Downloader:
                     'version': version
                 })
 
+        for excluded in excluded_deps:
+            print(f"Excluded Dependency: {excluded['artifactId']} (Group: {excluded['groupId']}) - {excluded['message']}")
+
         for dep in self.deps:
             print(f"Dependency: {dep['artifactId']}, Version: {dep['version']}")
 
-
     def extract_properties(self, data):
-        #tree = ET.parse(data)
         root = ET.fromstring(data)
         namespace = {'': 'http://maven.apache.org/POM/4.0.0'}
         properties_dict = {}
@@ -115,16 +136,14 @@ class Downloader:
             for prop in properties:
                 if prop.tag == '{http://maven.apache.org/POM/4.0.0}properties':
                     for child in prop:
-                        
                         tag_name = child.tag.split('}')[-1]
                         property_value = child.text.strip() if child.text else None
 
-                        property_value = self.resolve_placeholder(property_value,properties_dict)
+                        property_value = self.resolve_placeholder(property_value, properties_dict)
                         properties_dict[tag_name] = property_value
-                        #print(f"Property: {tag_name}, Value: {property_value}")
         return properties_dict
     
-    def extract_artifactId(self,root):
+    def extract_artifactId(self, root):
         namespace =  {'': 'http://maven.apache.org/POM/4.0.0'}
         artifact_id = root.find('artifactId', namespace)
         if artifact_id is not None:
@@ -141,10 +160,8 @@ class Downloader:
             if parent is not None:
                 version = parent.find('version', namespace)
         if version is not None:
-            #print(f"Project Version11: {version.text.strip()}")
             return version.text.strip()
         return None
-    
     
     def resolve_placeholder(self, value, properties):
         if value and isinstance(value, str):
@@ -161,8 +178,40 @@ class Downloader:
                         print(f"Warning: Placeholder '{placeholder}' not found in properties.")
                         break
         return value
+    
+    def write_files(self, file, content_to_write):
+        with open(file, "wb") as f:
+            f.write(content_to_write)
+    
+    
+    def download_files(self, download_location, dependencies):
+        if not os.path.exists(download_location):
+            os.makedirs(download_location)
+            print(f"Download Location Created: {download_location}")
+        
+        for dep in dependencies:
+            g_id = dep['groupId']
+            g_id_clean = g_id.replace(".", "/")
+            a_id = dep['artifactId']
+            version = dep['version']
+            
+            repo = f"https://repo1.maven.org/maven2/{g_id_clean}/{a_id}/{version}/{a_id}-{version}"
+            
+            filetypes = ["pom", "jar", "aar", "module"]
 
-
+            for filetype in filetypes:
+                filenm = repo+"."+filetype
+                response = requests.get(filenm, allow_redirects=True)
+                if response.status_code == 200:
+                    filename = filenm.split("https://repo1.maven.org/maven2/")[-1]
+                    file_path = os.path.join(download_location, filename)
+                    print(file_path)
+                    
+                    self.write_files(file_path, response.content)
+                else:
+                    print(f"Error getting file: {filenm}")       
+              
+    
 if __name__ == "__main__":
     dl = Downloader()
     
@@ -175,3 +224,6 @@ if __name__ == "__main__":
     dl.extract_dependencies(content, properties)
 
     print(f"\nPlugins: {len(dl.plugins)}\nDependencies: {len(dl.deps)}\n\nSum: {int(len(dl.deps)) + int(len(dl.plugins))}")
+    
+    print("===============\nLinks\n")
+    dl.download_files(dl.download_location, dl.deps)
